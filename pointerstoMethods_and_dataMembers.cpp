8aca3746720d8aba54b8ba6732d53cfd24844ac3
Anahit Ghazaryan <146667359+AnneGhazaryaan@users.noreply.github.com>
2024-02-03 15:28:03 +0400

    pointerstoMethods_and_dataMembers.cpp

---
+++ pointerstoMethods_and_dataMembers.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+
+class Point {
+public:
+    Point(int v) : m_v{v}, midpoint{0} {}
+    int getDot() const { return m_v; }
+    static void display() { std::cout << "-infinity  0  +infinity" << std::endl; }
+
+public:
+    inline static int unitLength = 1;
+    int midpoint;
+
+private:
+    int m_v;
+};
+
+int main() {
+    int (Point::*Fptr)() const = &Point::getDot;
+
+    Point A(4);
+    std::cout << (A.*Fptr)() << std::endl;
+
+    Point* p = new Point(3);
+    std::cout << (p->*Fptr)() << std::endl;
+
+
+    void (*staticMFptr)() = &Point::display;
+    staticMFptr();
+
+    int* staticVPtr = &Point::unitLength;
+    int Point::*nonStaticVPtr = &Point::midpoint;
+    std::cout << *staticVPtr << A.*nonStaticVPtr << std::endl;
+    
+    return 0;
+}
+
